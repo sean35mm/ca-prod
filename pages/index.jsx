69f467df84a986820cc9c06d82d51b0380778978
sean gil <sean35mm@gmail.com>
2022-06-09 20:31:11 -0700

    barebone preview

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import Head from 'next/head';
+import Image from 'next/image';
+import logo from '../src/assets/CA-Logo.png';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div>
-      <div>
-        <h1 className='font-panther text-2xl m-4'>Hello World!</h1>
-      </div>
-    </div>
-  )
+	return (
+		<div className='flex bg-primary px-4 w-full h-screen items-center justify-center'>
+			<div>
+				<h1 className='mt-10 text-accent font-panther text-2xl'>
+					Website Coming Soon!
+				</h1>
+				<ul>
+					<Link href='#'>
+						<li>Instagram</li>
+					</Link>
+					<Link href='#'>
+						<li>TikTok</li>
+					</Link>
+				</ul>
+				<Image src={logo} alt='logo' layout='intrinsic' />
+			</div>
+		</div>
+	);
 }
