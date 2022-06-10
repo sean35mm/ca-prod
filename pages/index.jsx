c1d5a2316fd1c625eba295fc5820c5175f0fbb99
sean gil <sean35mm@gmail.com>
2022-06-09 21:53:24 -0700

    add SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import logo from '../src/assets/CA-Logo.png';
+import Meta from '../src/components/ui/Meta';
 
 import { SiInstagram, SiTiktok } from 'react-icons/si';
 
@@ -9,34 +10,38 @@ const tiktok = 'https://www.tiktok.com/@child_appetite?lang=en';
 
 export default function Home() {
 	return (
-		<div className='flex bg-primary px-4 w-full h-screen items-center justify-center'>
-			<div>
-				<Image
-					src={logo}
-					alt='logo'
-					width='400px'
-					height='200px'
-					layout='responsive'
-				/>
-				<h1 className='mt-10 text-accent font-panther text-3xl text-center tracking-widest'>
-					Website
-				</h1>
-				<h1 className='text-accent font-panther text-3xl text-center tracking-widest'>
-					Coming Soon!
-				</h1>
-				<ul className='flex text-center mt-4 mb-24 justify-center'>
-					<a href={instagram} target='_blank' rel='noopener noreferrer'>
-						<li className='icon'>
-							<SiInstagram color='#FFF5D2' size={'40px'} />
-						</li>
-					</a>
-					<a href={tiktok} target='_blank' rel='noopener noreferrer'>
-						<li className='icon'>
-							<SiTiktok color='#FFF5D2' size={'40px'} />
-						</li>
-					</a>
-				</ul>
+		<>
+			<Meta />
+
+			<div className='flex bg-primary px-4 w-full h-screen items-center justify-center'>
+				<div>
+					<Image
+						src={logo}
+						alt='logo'
+						width='400px'
+						height='200px'
+						layout='responsive'
+					/>
+					<h1 className='mt-10 text-accent font-panther text-3xl text-center tracking-widest'>
+						Store
+					</h1>
+					<h1 className='text-accent font-panther text-3xl text-center tracking-widest'>
+						Launching Soon!
+					</h1>
+					<ul className='flex text-center mt-4 mb-24 justify-center'>
+						<a href={instagram} target='_blank' rel='noopener noreferrer'>
+							<li className='icon'>
+								<SiInstagram color='#FFF5D2' size={'40px'} />
+							</li>
+						</a>
+						<a href={tiktok} target='_blank' rel='noopener noreferrer'>
+							<li className='icon'>
+								<SiTiktok color='#FFF5D2' size={'40px'} />
+							</li>
+						</a>
+					</ul>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
 
-import Navbar from '../Navbar'
+import Navbar from '../Navbar';
 
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/site.webmanifest' />
+			</Head>
+			<Navbar />
+			<main>{children}</main>
+		</>
+	);
+};
 
-
-const Layout = ({children}) => {
-  return (
-    <>
-      <Head>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Navbar />
-      <main>{children}</main>
-    </>
-  )
-}
-
-export default Layout
+export default Layout;
